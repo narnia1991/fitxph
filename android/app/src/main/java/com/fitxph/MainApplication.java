e6a7d99f362a2dcd170a2501e9d7e5f491abfbe1
creephanger <jeanaica.suplido@gmail.com>
2018-03-25 21:39:55 +0800

    :bug: Fix links to app

--- android/app/src/main/java/com/fitxph/MainApplication.java
+++ android/app/src/main/java/com/fitxph/MainApplication.java
@@ -27,9 +27,9 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new VectorIconsPackage(),
             new TextToSpeechPackage(),
             new BlePackage(),
+            new VectorIconsPackage(),
             new SvgPackage()
       );
     }
