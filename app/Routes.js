d90feda9f7abb69282eb7af6fae47e80505e9f62
Jeanaica Suplido <creephanger@gmail.com>
2018-04-01 19:40:32 +0800

    :art: Progress
    fix routes

--- app/Routes.js
+++ app/Routes.js
@@ -3,11 +3,14 @@ import { Container, Root, StyleProvider } from "native-base";
 import { Router, Scene } from "react-native-router-flux";
 import { Landing, Login, Reference, SignUp, Splash, Sync, SyncNow } from './screens/General';
 import { PlanAdd, PlanList } from './screens/Plan';
+import { InitialData } from './screens/Profile';
+
 import { Nav, NavBar } from "./components";
 import getTheme from '../native-base-theme/components';
 import material from '../native-base-theme/variables/material';
 
 class routes extends React.Component {
+
   render() {
     return (
       <Root>
@@ -40,97 +43,102 @@ class routes extends React.Component {
                 <Scene key="syncnow"
                   component={SyncNow} hideNavBar={true} passProps />
 
-                {/*
-                Plan
-                */}
                 <Scene key="plan"
                   title="Plan"
-                  initial={true}
                   navBar={Nav}
                   component={PlanList} passProps />
                 <Scene key="planAdd"
                   title="Add a Plan"
                   hideNavBar={true}
                   component={PlanAdd} passProps />
+
+                <Scene
+                  key="initialData"
+                  initial={true}
+                  component={InitialData}
+                  hideNavBar={true}
+                  passProps
+                />
+
                 {/*
-           <Scene
-            key="calendar"
-            component={Calendar}
-            hideNavBar={true}
-            passProps
-          />
+                <Scene
+                  key="calendar"
+                  component={Calendar}
+                  hideNavBar={true}
+                  passProps
+                />
 
-          <Scene
-            key="reference"
-            component={Reference}
-            hideNavBar={true}
-            passProps
-          />
+                <Scene
+                  key="reference"
+                  component={Reference}
+                  hideNavBar={true}
+                  passProps
+                />
 
-          <Scene key="day" component={Day} hideNavBar={true} passProps />
-          <Scene
-            key="exercise"
-            component={Exercise}
-            hideNavBar={true}
-            passProps
-          />
-          <Scene
-            key="exercisefinished"
-            component={ExerciseFinished}
-            hideNavBar={true}
-            passProps
-          />
-          <Scene
-            key="exerciseongoing"
-            component={ExerciseOnGoing}
-            hideNavBar={true}
-            passProps
-          />
-          <Scene
-            key="featured"
-            component={Featured}
-            hideNavBar={true}
-            passProps
-          />
-          <Scene key="food" component={Food} hideNavBar={true} passProps />
-          <Scene
-            key="foodadd"
-            component={FoodAdd}
-            hideNavBar={true}
-            passProps
-          />
-          <Scene
-            key="foodlist"
-            component={FoodList}
-            hideNavBar={true}
-            passProps
-          />
-          <Scene
-            key="initialdata"
-            component={InitialData}
-            hideNavBar={true}
-            passProps
-          />
-          <Scene key="plan" component={Plan} hideNavBar={true} passProps />
-          <Scene
-            key="planlist"
-            component={PlanList}
-            hideNavBar={true}
-            passProps
-          />
-          <Scene
-            key="progress"
-            component={Progress}
-            hideNavBar={true}
-            passProps
-          />
-          <Scene key="syncnow" component={SyncNow} hideNavBar={true} />
-          <Scene
-            key="exerciselist"
-            component={ExerciseList}
-            hideNavBar={true}
-            passProps
-          /> */}
+                <Scene key="day" component={Day} hideNavBar={true} passProps />
+                <Scene
+                  key="exercise"
+                  component={Exercise}
+                  hideNavBar={true}
+                  passProps
+                />
+                <Scene
+                  key="exercisefinished"
+                  component={ExerciseFinished}
+                  hideNavBar={true}
+                  passProps
+                />
+                <Scene
+                  key="exerciseongoing"
+                  component={ExerciseOnGoing}
+                  hideNavBar={true}
+                  passProps
+                />
+                <Scene
+                  key="featured"
+                  component={Featured}
+                  hideNavBar={true}
+                  passProps
+                />
+                <Scene key="food" component={Food} hideNavBar={true} passProps />
+                <Scene
+                  key="foodadd"
+                  component={FoodAdd}
+                  hideNavBar={true}
+                  passProps
+                />
+                <Scene
+                  key="foodlist"
+                  component={FoodList}
+                  hideNavBar={true}
+                  passProps
+                />
+                <Scene
+                  key="initialdata"
+                  component={InitialData}
+                  hideNavBar={true}
+                  passProps
+                />
+                <Scene key="plan" component={Plan} hideNavBar={true} passProps />
+                <Scene
+                  key="planlist"
+                  component={PlanList}
+                  hideNavBar={true}
+                  passProps
+                />
+                <Scene
+                  key="progress"
+                  component={Progress}
+                  hideNavBar={true}
+                  passProps
+                />
+                <Scene key="syncnow" component={SyncNow} hideNavBar={true} />
+                <Scene
+                  key="exerciselist"
+                  component={ExerciseList}
+                  hideNavBar={true}
+                  passProps
+                /> */}
               </Scene>
             </Router>
           </Container>
--- app/screens/Profile/InitialData.js
+++ app/screens/Profile/InitialData.js
@@ -2,16 +2,8 @@
 // height | initial weight | target weight
 import React from "react";
 import { Actions } from "react-native-router-flux";
-import {
-  Container,
-  Content,
-  Form,
-  Item,
-  Input,
-  Label,
-  Text,
-  Button
-} from "native-base";
+import { Form, Text } from "native-base";
+import { ScreenLabel, Submit, TextBox, Wrapper } from "../../components";
 
 class InitialData extends React.Component {
   state = {
@@ -19,12 +11,12 @@ class InitialData extends React.Component {
     gender: ""
   };
 
-  componentWillMount = () => {
-    if (!this.props.user) {
-      Actions.login();
-    }
-    this.setState({ user: this.props.user });
-  };
+  // componentWillMount = () => {
+  //   if (!this.props.user) {
+  //     Actions.login();
+  //   }
+  //   this.setState({ user: this.props.user });
+  // };
 
   onValueChange(value) {
     this.setState({
@@ -37,49 +29,57 @@ class InitialData extends React.Component {
   }
 
   render() {
-    return (
-      <Container>
-        <Content>
-          <Form>
-            <Item floatingLabel>
-              <Label>What is your name?</Label>
-              <Input />
-            </Item>
-            <Item floatingLabel>
-              <Label>What is your date of birth?(MM/DD/YYYY)</Label>
-              <Input />
-            </Item>
-            <Item floatingLabel>
-              <Label>What is your current weight?</Label>
-              <Input />
-            </Item>
-            <Item floatingLabel>
-              <Label>What is your height?</Label>
-              <Input />
-            </Item>
-            <Item stackedLabel>
-              <Label>What is your gender?</Label>
-              <Picker
-                mode="dropdown"
-                placeholder="Select Gender"
-                onValueChange={this.onValueChange.bind(this)}
-              >
-                <Item label="Male" value="male" />
-                <Item label="Female" value="female" />
-              </Picker>
-            </Item>
-            <Item floatingLabel last>
-              <Label>What is your weight goal?</Label>
-              <Input />
-            </Item>
-            <Button block onPress={this.handleSubmit}>
-              <Text>Submit</Text>
-            </Button>
-          </Form>
-        </Content>
-      </Container>
-    );
+    return [
+      <Wrapper key={1}>
+        <ScreenLabel text="Let's get started!" />
+        <Form>
+          <TextBox label="Name" />
+        </Form>
+      </Wrapper>,
+      <Submit key={2} text="Submit" onSubmit={this.handleSubmit} />
+    ];
   }
 }
 
 export default InitialData;
+
+{/* <Container>
+  <Content>
+    <Form>
+      <Item floatingLabel>
+        <Label>What is your name?</Label>
+        <Input />
+      </Item>
+      <Item floatingLabel>
+        <Label>What is your date of birth?(MM/DD/YYYY)</Label>
+        <Input />
+      </Item>
+      <Item floatingLabel>
+        <Label>What is your current weight?</Label>
+        <Input />
+      </Item>
+      <Item floatingLabel>
+        <Label>What is your height?</Label>
+        <Input />
+      </Item>
+      <Item stackedLabel>
+        <Label>What is your gender?</Label>
+        <Picker
+          mode="dropdown"
+          placeholder="Select Gender"
+          onValueChange={this.onValueChange.bind(this)}
+        >
+          <Item label="Male" value="male" />
+          <Item label="Female" value="female" />
+        </Picker>
+      </Item>
+      <Item floatingLabel last>
+        <Label>What is your weight goal?</Label>
+        <Input />
+      </Item>
+      <Button block onPress={this.handleSubmit}>
+        <Text>Submit</Text>
+      </Button>
+    </Form>
+  </Content>
+</Container> */}
